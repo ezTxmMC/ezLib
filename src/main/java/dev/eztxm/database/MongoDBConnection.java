e8d460f05e1d48ead15427816b7e4e0130865785
ezTxmMC <eztxmmc@hotmail.com>
2024-04-22 21:40:58 +0200

    added mongodb (not final)

--- src/main/java/dev/eztxm/database/MongoDBConnection.java
+++ src/main/java/dev/eztxm/database/MongoDBConnection.java
@@ -1,4 +1,44 @@
 package dev.eztxm.database;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class MongoDBConnection {
+    private final MongoDatabase mongoDatabase;
+
+    public MongoDBConnection(String host, int port, String username, String password, String database) {
+        String uri = "mongodb://" + username + ":" + password + "@" + host + ":" + port;
+        MongoClient mongoClient = MongoClients.create(uri);
+        mongoDatabase = mongoClient.getDatabase(database);
+    }
+
+    public boolean createCollection(String name) {
+        if (!isCollectionExists(name)) return false;
+        mongoDatabase.createCollection(name);
+        return true;
+    }
+
+    public boolean isCollectionExists(String name) {
+        AtomicBoolean exists = new AtomicBoolean(false);
+        mongoDatabase.listCollectionNames().forEach(string -> {
+            if (!string.equals(name)) {
+                return;
+            }
+            exists.set(true);
+        });
+        return exists.get();
+    }
+
+    public MongoCollection<Document> getCollection(String name) {
+        return mongoDatabase.getCollection(name);
+    }
+
+    public void set(String collection) {
+
+    }
 }
