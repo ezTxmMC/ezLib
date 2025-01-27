33287679731e6826aa48449523cb79d88b698776
ezTxmMC <eztxmmc@hotmail.com>
2025-01-27 18:02:39 +0100

    optimized arguments loop

--- ezlib-database-core/src/main/java/de/eztxm/ezlib/database/Arguments.java
+++ ezlib-database-core/src/main/java/de/eztxm/ezlib/database/Arguments.java
@@ -12,27 +12,35 @@ public class Arguments {
             Object object = objects[i];
             if (object instanceof String) {
                 preparedStatement.setString(i + 1, (String)object);
+                continue;
             }
             if (object instanceof Integer) {
                 preparedStatement.setInt(i + 1, (Integer) object);
+                continue;
             }
             if (object instanceof Date) {
                 preparedStatement.setDate(i + 1, (Date)object);
+                continue;
             }
             if (object instanceof Timestamp) {
                 preparedStatement.setTimestamp(i + 1, (Timestamp)object);
+                continue;
             }
             if (object instanceof Boolean) {
                 preparedStatement.setBoolean(i + 1, (Boolean) object);
+                continue;
             }
             if (object instanceof Float) {
                 preparedStatement.setFloat(i + 1, (Float) object);
+                continue;
             }
             if (object instanceof Double) {
                 preparedStatement.setDouble(i + 1, (Double) object);
+                continue;
             }
             if (object instanceof Long) {
                 preparedStatement.setLong(i + 1, (Long) object);
+                continue;
             }
         }
     }
